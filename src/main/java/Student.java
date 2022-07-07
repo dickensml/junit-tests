adc0ead2a24d8bd7d42d79c40db7dd41b85dc404
Michael Dickens <dickens.michael64@gmail.com>
2022-07-07 17:50:17 -0500

    finally got this up

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,37 +1,50 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+
 public class Student {
-
     private long id;
-
     private String name;
+    private ArrayList<Integer> grades;
+    public Student() {
+        this.grades = new ArrayList<>();}
 
-    private ArrayLIst<Integer> grades;
-
-    public Student(){}
-
-    public Student(long id, String name){
+    public Student(long id, String name) {
         this.id = id;
         this.name = name;
-    }
+        this.grades = new ArrayList<>();}
+
     public Student(long id, String name, ArrayList<Integer> grades) {
         this.id = id;
         this.name = name;
-        this.grades = grades;
+        this.grades = grades;}
+
+    public long getId() {
+        return id;}
+
+    public void setId(long id) {
+        this.id = id;}
+
+    public String getName() {
+        return name;}
+
+    public void setName(String name) {
+        this.name = name;}
+
+    public ArrayList<Integer> getGrades() {
+        return grades;}
+
+    public void setGrades(ArrayList<Integer> grades) {
+        this.grades = grades;}
+
+    public void addGrade(int grade){
+        this.grades.add(grade);}
+
+    public double getGradeAverage(){
+        double total = 0;
+        for(int grade: grades){
+            total += grade;
+        }
+        double average = total / grades.size();
+
+        return average;
     }
-
-    // returns the student's id
-    public long getId(){...}
-
-    // returns the student's name
-    public String getName(){...}
-
-    // adds the given grade to the grades list
-    public void addGrade(int grade){...}
-
-    // returns the list of grades
-    public ArrayList<Integer> getGrades(){...}
-
-    // returns the average of the students grades
-    public double getGradeAverage(){...}
-
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,7 +1,39 @@
-import org.junit.Tests;
+import org.junit.Test;
 import static org.junit.Assert.*;
 import java.util.ArrayList;
+
 public class StudentTest {
-
-
+    @Test
+    public void testCreateStudent() {
+        Student name = new Student(1L, "Roberto");
+        Student otherPerson = null;
+        assertNull(otherPerson);
+        assertNotNull(name);
+    }
+    @Test
+    public void testAllFieldsGetUpdated() {
+        Student name = new Student(1L, "Katy");
+        assertSame(1L, name.getId());
+        assertSame("Katy", name.getName());
+        assertSame(0, name.getGrades().size());
+        Student name2 = new Student();
+        assertSame(0, name2.getGrades().size());
+    }
+    @Test
+    public void testAddingGradesWorks() {
+        Student name = new Student(1L, "Bob");
+        name.addGrade(100);
+        assertSame(100, name.getGrades().get(0));
+        name.addGrade(80);
+        assertSame(2, name.getGrades().size());
+    }
+    @Test
+    public void testAverageGradeWorks() {
+        Student name = new Student(1L, "MikeD");
+        name.addGrade(100);
+        name.addGrade(100);
+        assertEquals(100, name.getGradeAverage(), 0);
+        name.addGrade(50);
+        assertEquals(83.3, name.getGradeAverage(), 0.04);
+    }
 }
